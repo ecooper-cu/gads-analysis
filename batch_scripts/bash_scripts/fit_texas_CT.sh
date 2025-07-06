4ae87214b85f82c5b3a65d5ab1a4905b1bc9c215
Emma Cooper <emco4286@c3cpu-c15-u34-3.rc.int.colorado.edu>
2025-07-06 14:51:26 -0600

    adjustements to bash scripts

--- batch_scripts/bash_scripts/fit_texas_CT.sh
+++ batch_scripts/bash_scripts/fit_texas_CT.sh
@@ -1,18 +1,31 @@
 #!/bin/bash
 
 #SBATCH --account=ucb678_asc1
-#SBATCH --partition=amilan
+#SBATCH --partition=aa100
 #SBATCH --job-name=fit_texas_CT
 #SBATCH --output=out/ercot_markov_fitting/CT.%j.out
 #SBATCH --time=24:00:00
 #SBATCH --qos=normal
+#SBATCH --gres=gpu:1
 #SBATCH --nodes=1
-#SBATCH --mem=0
+#SBATCH --ntasks=8
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=emco4286@colorado.edu
 
 module purge
-module load python
+module load python/3.10.2
+module load cuda/11.8
+
+# Start GPU monitoring in the background
+nvidia-smi -l 300 >> gpu_usage-jobid-${SLURM_JOB_ID}.log &
+MONITOR_PID=$!
+
+seff ${SLURM_JOB_ID} -l 300 >> cpu_usage-jobid-${SLURM_JOB_ID}.log &
+MONITOR_PID=$!
+
+nvcc -o vectorAdd vectorAdd.cu
+nsys profile --trace=cuda,osrt ./vectorAdd
 
 source /projects/emco4286/environments/fitting/bin/activate
+echo "Running .py script"
 python /home/emco4286/gads-analysis/batch_scripts/python_scripts/fit_texas_CT.py
